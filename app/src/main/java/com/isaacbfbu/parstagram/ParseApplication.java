393a2fdad6393fc5d6ad356ce4e78d6034dcecfa
bermani <bermani@uci.edu>
2020-07-07 22:35:04 -0700

    Implement login and app icon

---
+++ app/src/main/java/com/isaacbfbu/parstagram/ParseApplication.java
@@ -0,0 +1,35 @@
+package com.isaacbfbu.parstagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Use for troubleshooting -- remove this line for production
+        Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
+
+        // Use for monitoring Parse OkHttp traffic
+        // Can be Level.BASIC, Level.HEADERS, or Level.BODY
+        // See http://square.github.io/okhttp/3.x/logging-interceptor/ to see the options.
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.networkInterceptors().add(httpLoggingInterceptor);
+
+        // set applicationId, and server server based on the values in the Heroku settings.
+        // clientKey is not needed unless explicitly configured
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("isaac-parstagram") // should correspond to APP_ID env variable
+                .clientKey("E697C774902E5C9D865FD97F9FEA685C77D6D656F12E194DC074FEC1E7D7B248")  // set explicitly unless clientKey is explicitly configured on Parse server
+                .clientBuilder(builder)
+                .server("https://isaac-parstagram.herokuapp.com/parse/").build());
+    }
+}
