724156de64295172f1db64e11594e9c72c3bfe45
bermani <bermani@uci.edu>
2020-07-08 00:41:23 -0700

    Implement compose fragment layout

--- app/src/main/java/com/isaacbfbu/parstagram/fragments/ComposeFragment.java
+++ app/src/main/java/com/isaacbfbu/parstagram/fragments/ComposeFragment.java
@@ -1,16 +1,24 @@
 package com.isaacbfbu.parstagram.fragments;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 
 import com.isaacbfbu.parstagram.R;
+import com.isaacbfbu.parstagram.databinding.FragmentComposeBinding;
 
 public class ComposeFragment extends Fragment {
+
+    FragmentComposeBinding binding;
 
     public ComposeFragment() {
         // Required empty public constructor
@@ -21,6 +29,22 @@ public class ComposeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_compose, container, false);
+        binding = FragmentComposeBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.ivUploaded.setMinimumHeight(Resources.getSystem().getDisplayMetrics().widthPixels);
+        binding.etDescription.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        binding.etDescription.setRawInputType(InputType.TYPE_CLASS_TEXT);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
