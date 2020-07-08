d0bbe71ffd248fa09a2f182ff8d95667c342a8a8
bermani <bermani@uci.edu>
2020-07-07 23:46:44 -0700

    Implement bottom navigation with fragments

--- app/src/main/java/com/isaacbfbu/parstagram/MainActivity.java
+++ app/src/main/java/com/isaacbfbu/parstagram/MainActivity.java
@@ -1,18 +1,62 @@
 package com.isaacbfbu.parstagram;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.isaacbfbu.parstagram.databinding.ActivityMainBinding;
+import com.isaacbfbu.parstagram.fragments.ComposeFragment;
+import com.isaacbfbu.parstagram.fragments.HomeFragment;
+import com.isaacbfbu.parstagram.fragments.ProfileFragment;
 import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // define your fragments here
+        final Fragment homeFragment = new HomeFragment();
+        final Fragment composeFragment = new ComposeFragment();
+        final Fragment profileFragment = new ProfileFragment();
+
+        // handle navigation selection
+        binding.bottomNavigation.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment fragment;
+                        switch (item.getItemId()) {
+                            case R.id.action_home:
+                                fragment = homeFragment;
+                                break;
+                            case R.id.action_compose:
+                                fragment = composeFragment;
+                                break;
+                            case R.id.action_profile:
+                            default:
+                                fragment = profileFragment;
+                                break;
+                        }
+                        fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                        return true;
+                    }
+                });
+        // Set default selection
+        binding.bottomNavigation.setSelectedItemId(R.id.action_home);
     }
 
     public void logout(View v) {
---
+++ app/src/main/java/com/isaacbfbu/parstagram/fragments/ComposeFragment.java
@@ -0,0 +1,26 @@
+package com.isaacbfbu.parstagram.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.isaacbfbu.parstagram.R;
+
+public class ComposeFragment extends Fragment {
+
+    public ComposeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_compose, container, false);
+    }
+}
